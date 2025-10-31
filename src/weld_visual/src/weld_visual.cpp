766e1c553b58e569880cf5627f77b1c198a14899
tong <tong166159@163.com>
2025-10-31 11:48:38 +0800

    remove unnecessary dependencies

--- src/weld_visual/src/weld_visual.cpp
+++ src/weld_visual/src/weld_visual.cpp
@@ -6,10 +6,6 @@
 #include <rclcpp/rclcpp.hpp>
 #include <geometry_msgs/msg/pose.hpp>
 #include <moveit_visual_tools/moveit_visual_tools.h>
-// #include <moveit_msgs/msg/display_robot_state.hpp>
-// #include <moveit_msgs/msg/display_trajectory.hpp>
-// #include <moveit_msgs/msg/attached_collision_object.hpp>
-// #include <moveit_msgs/msg/collision_object.hpp>
 
 using namespace std;
 
